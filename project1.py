935f4dcf7af3f6dc547a3ee1ebebd50b1c7a8723
Treyhealingeyes86 <karinafriesen@seattleacademy.org>
2026-09-14 21:28:21 +0000

    Unfinished project 1

---
+++ project1.py
@@ -0,0 +1,18 @@
+print ("Hello! This is a story about a young adventurer.")
+name = input ("First enter the name of this adventurer: ")
+if name == "Trey":
+    print ("WOW! I LOVE that name!!!!!! But now name the town they live in.")
+else:
+    print (f"{name}! What an amazing name! Now, name the town that {name} is coming from.")
+town = input ("Enter here: ")
+print (f"Okay, last few things so that {name} can begin their journey.")
+companion = input ("Please enter the name of their companion: ")
+food = input (f"Enter {name}'s favorite food: ")
+if food == "chocolate milk":
+    print ("OH WOW! ME TOO!")
+    print (f"Ok, moving on, {name} and {companion} are leaving {town} to embark on an awesome journey.")
+else:
+    print (f"Ok so {name} and {companion} are leaving {town} to embark on an awesome journey.")
+input ("Enter when ready: ")
+print (f"One day, {name} was on their daily walk in {town} when they discovered a...")
+discovery1 = input ("")
